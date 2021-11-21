801115c39770d9a504fe1a355f4eba7a9979efb1
nbur4556 <nbur4556@gmail.com>
2021-11-20 21:39:16 -0600

    feat: send output from neuron between positive and negative 1

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const Neuron = require('./neuron.js');
 
 const neuron = new Neuron();
+const neuron2 = new Neuron(.0777);
+
+console.log("SigOut:", neuron.outputSignal(1));
+console.log("SigOut:", neuron.outputSignal(2));
+
+console.log("SigOut:", neuron2.outputSignal(1));
+console.log("SigOut:", neuron2.outputSignal(2));
--- neuron.js
+++ neuron.js
@@ -1,5 +1,15 @@
-function Neuron() {
-    this.weight = Math.random();
+function Neuron(weight = null) {
+    this.weight = weight || (Math.random() * 2) - 1;
+
+    this.sigmoidSquash = function (x) {
+        const e = 2.71828;
+        return (2 / (1 + Math.pow(e, x * -1))) - 1;
+    }
 }
 
-module.exports = Neuron;
+Neuron.prototype.outputSignal = function (input) {
+    const output = input * this.weight;
+    return this.sigmoidSquash(output);
+}
+
+module.exports = Neuron; gi
