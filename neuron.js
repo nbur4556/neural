239001d96e7643520cd01fd2f677313aaa86032e
nbur4556 <nbur4556@gmail.com>
2021-11-20 21:47:56 -0600

    feat: use a dynamic sigmoid squash function

--- neuron.js
+++ neuron.js
@@ -1,15 +1,16 @@
 function Neuron(weight = null) {
     this.weight = weight || (Math.random() * 2) - 1;
-
-    this.sigmoidSquash = function (x) {
-        const e = 2.71828;
-        return (2 / (1 + Math.pow(e, x * -1))) - 1;
-    }
 }
 
 Neuron.prototype.outputSignal = function (input) {
     const output = input * this.weight;
-    return this.sigmoidSquash(output);
+    return sigmoidSquash(output);
 }
 
 module.exports = Neuron; gi
+
+// Squash a number between 1 and -1 on an S curve
+const sigmoidSquash = (x, range = 2, slope = -2, offset = -1) => {
+    const e = 2.71828; // Euler's Number
+    return (range / (1 + Math.pow(e, slope * x))) + offset;
+}
